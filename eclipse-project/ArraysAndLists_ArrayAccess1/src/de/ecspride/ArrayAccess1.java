a86bd98ce8915006641a91bb1e4067299cebdafa
cfritz <christian@fritz-online.org>
2013-05-06 14:53:42 +0200

    comment

--- eclipse-project/ArraysAndLists_ArrayAccess1/src/de/ecspride/ArrayAccess1.java
+++ eclipse-project/ArraysAndLists_ArrayAccess1/src/de/ecspride/ArrayAccess1.java
@@ -5,7 +5,20 @@ import android.content.Context;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
-
+/**
+ * @testcase_name ArrayAccess1
+ * @version 0.1
+ * @author Secure Software Engineering Group (SSE), European Center for Security and Privacy by Design (EC SPRIDE) 
+ * @author_mail siegfried.rasthofer@cased.de
+ * 
+ * @description an array is created which is filled with untainted and tainted (deviceId source) data.
+ *  The untainted data is sent via sms.
+ * @dataflow -
+ * @number_of_leaks 0
+ * @challenges the analysis must distinguish between different array positions to recognize that the tainted
+ *  data does not get leaked. 
+ *
+ */
 public class ArrayAccess1 extends Activity {
 	public static String[] arrayData;
 	
@@ -17,13 +30,13 @@ public class ArrayAccess1 extends Activity {
         arrayData = new String[3];
 		
 		arrayData[0] = "element 1 is tainted:";
-		arrayData[1] = ((TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId();
+		arrayData[1] = ((TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId(); //source
 		//arrayData[2] is not tainted
 		arrayData[2] = "neutral text";
 		
 		SmsManager sms = SmsManager.getDefault();
 		
 		//no data leak: 3rd argument of sendTextmessage() is not tainted
-        sms.sendTextMessage("+49 1234", null, arrayData[2], null, null);  
+        sms.sendTextMessage("+49 1234", null, arrayData[2], null, null);  //sink
     }    
 }
