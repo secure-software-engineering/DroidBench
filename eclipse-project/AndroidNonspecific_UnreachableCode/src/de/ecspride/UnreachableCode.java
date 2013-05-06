8791f2533e02726985ed8c751b81afdf44addca1
cfritz <christian@fritz-online.org>
2013-05-07 00:22:25 +0200

    comment

--- eclipse-project/AndroidNonspecific_UnreachableCode/src/de/ecspride/UnreachableCode.java
+++ eclipse-project/AndroidNonspecific_UnreachableCode/src/de/ecspride/UnreachableCode.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 /**
- * @testcase_name LogNoLeak
+ * @testcase_name UnreachableCode
  * @version 0.1
  * @author Secure Software Engineering Group (SSE), European Center for Security and Privacy by Design (EC SPRIDE) 
  * @author_mail siegfried.rasthofer@cased.de
