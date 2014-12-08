c7edf98aa77513f12efd2c86c9ac05028330210b
Nguyen Nguyen <nguyen@uwinsoftware.com>
2014-12-08 11:31:13 -0500

    Added ICC-ComponentName-Class-Constant

---
+++ ndss-2015/ICC-ComponentName-Class-Constant/src/edu/mit/OutFlowActivity.java
@@ -0,0 +1,30 @@
+package edu.mit;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import java.util.Random;
+
+import lu.uni.serval.icc_startactivity4.R;
+import android.content.ComponentName;
+
+public class OutFlowActivity extends Activity {
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+	
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String imei = telephonyManager.getDeviceId(); //source
+	
+        ComponentName comp = new ComponentName(getPackageName(), InFlowActivity.class.getName());
+        Intent i = new Intent().setComponent(comp);
+        i.putExtra("DroidBench", imei);
+	
+        startActivity(i);
+    }
+}
