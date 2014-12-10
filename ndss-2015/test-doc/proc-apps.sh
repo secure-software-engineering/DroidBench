2394e1cf7e68a43c5e8a353527d39e9f1c3413d8
Nguyen Nguyen <nguyen@uwinsoftware.com>
2014-12-10 11:44:54 -0500

    Added DroidBench.xml templates

---
+++ ndss-2015/test-doc/proc-apps.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+APPS='
+Activity-Asynchronous-Event-Ordering
+Activity-Saved-State
+Application-Modeling
+ArrayCopy
+ArraySlice
+Button-Object-Allocation
+Clinit
+Clone
+Dynamic-Dispatch
+Event-Context-Shared-Pref-Listener
+Event-Ordering
+Fragments
+ICC-Action-String-Operations
+ICC-Broadcast-Programmatic-IntentFilter
+ICC-Component-Not-in-Manifest
+ICC-Concat-Action-String
+ICC-Intent-Component-Name
+ICC-Intent-Passed-Through-API
+ICC-Non-Constant-Class-Object
+ICC-Pass-Action-String-Through-API
+ICC-Service-Messages
+ICC-Unresolvable-Intent
+Intent-Class-Modeling
+Non-Sink-Argument-Flow
+OutputStream
+Parcel
+Pattern-Matcher
+Public-API-Field
+Serialization
+Service-Lifecycle
+SharedPreferences
+StringFormatter
+String-to-Char
+ToString
+Two-Components-Share-Memory
+'
+
+#APPS=$APPS_NON_ICC
+MAKE_TEMPLATE=../test-doc/make.template
+SCRIPTNAME=`basename $0`
+SCRIPTDIR=`dirname $0` 
+APPS=`cat $SCRIPTDIR/apps.txt`
+SCRIPTDIR=`(cd $SCRIPTDIR && pwd)`
+if [ "$#" -eq 0 ]; then
+    echo "$SCRIPTNAME makefile | project | build | apk-setup | check-info | specdump | create-expected | gen-review | gen-droidbench"
+    exit -1
+fi 
+
+CMD=$1
+for APP in $APPS; do
+    #runds dump $APP;
+    #echo $APP
+    case $CMD in
+        makefile)
+        (cd $APP; echo "NAME:= $APP" > Makefile; cat $MAKE_TEMPLATE >> Makefile);;
+
+        create-expected)
+        (cd $APP; grep FLOW droidsafe-gen/info-flow-results.txt >  expected-info-flows.txt);;
+
+        gen-droidbench)
+        (cd $APP; mkdir -p conf && cat ../test-doc/DroidBench.xml | sed "s/TEST_CASE/$APP/" > conf/DroidBench.xml);;
+
+        gen-review)
+        echo "######################"
+	    echo "STATUS:   OFFICIAL:   POS:   NEG:   MISSED:"
+        echo $APP/expected-info-flows.txt
+        echo $APP/droidsafe-gen/info-flow-results.txt
+        echo " "
+        ;;
+
+        project)
+            (cd $APP; rm -f build.xml; make update);;
+
+        build)
+            (cd $APP; make build);;
+
+        apk-setup)
+            (mkdir -p apk/$APP; cd apk/$APP; ln -sf ../../$APP/expected-info-flows.txt; ln -sf ../../$APP/bin/$APP-debug.apk $APP.apk;
+             ln -sf ../../$APP/Makefile . ; ln -sf ../../$APP/build.xml .) ;;
+
+        apk-remove)
+            (cd $APP; git rm $APP.apk);;
+        *)
+           echo "invalid $CMD "
+           exit -1 
+    esac
+done
