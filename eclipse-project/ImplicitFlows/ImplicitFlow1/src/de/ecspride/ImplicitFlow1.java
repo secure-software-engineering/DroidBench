8d7aa8ee168faa7cdcd90d5385359ddb17887e62
Siegfried Rasthofer <siegfried.rasthofer@cased.de>
2015-02-24 13:19:21 +0100

    updated ImplicitFlow1 example

--- eclipse-project/ImplicitFlows/ImplicitFlow1/src/de/ecspride/ImplicitFlow1.java
+++ eclipse-project/ImplicitFlows/ImplicitFlow1/src/de/ecspride/ImplicitFlow1.java
@@ -28,8 +28,8 @@ public class ImplicitFlow1 extends Activity {
 		String obfuscatedIMEI = obfuscateIMEI(imei);
 		writeToLog(obfuscatedIMEI);
 		
-		//hard to detect
-		obfuscatedIMEI = reallyHardObfuscatedIMEI(imei);
+		//hard to detect (implicit flow)
+		obfuscatedIMEI = copyIMEI(imei);
 		writeToLog(obfuscatedIMEI); 
 		
 	}
@@ -55,21 +55,22 @@ public class ImplicitFlow1 extends Activity {
 		return result;
 	}
 	
-	private String reallyHardObfuscatedIMEI(String imei){
+	private String copyIMEI(String imei){
 		//ASCII values for integer: 48-57
 		Integer[] numbers = new Integer[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,
 				20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,
-				40,4142,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57};
+				40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57};
 		
 		char[] imeiAsChar = imei.toCharArray();
 		
 		char[] newOldIMEI = new char[imeiAsChar.length];
 		
 		for(int i = 0; i < imeiAsChar.length; i++){
-			newOldIMEI[i] = Character.forDigit(numbers[(int)imeiAsChar[i]], 10);
+			int tmp = numbers[(int)imeiAsChar[i]];
+			newOldIMEI[i] = (char)tmp;
 		}
 		
-		return newOldIMEI.toString();
+		return new String (newOldIMEI);
 	}
 	
 	private void writeToLog(String message){
