afcc73cce9c445edc52046574c11d1dd1e70de2e
L-Jubarah-upb <leen.jubarah@upb.de>
2025-05-30 17:04:24 +0200

    Update AnnonymousClass1.java

--- eclipse-project/Callbacks/AnonymousClass1/src/de/ecspride/AnnonymousClass1.java
+++ eclipse-project/Callbacks/AnonymousClass1/src/de/ecspride/AnnonymousClass1.java
@@ -42,8 +42,8 @@ public class AnnonymousClass1 extends Activity {
 		@Override
 		public void onLocationChanged(Location location) { //source
 			Toast.makeText(getApplicationContext(), "aa", Toast.LENGTH_LONG).show();
-			latitude = location.getLatitude();
-			longitude = location.getLongitude();		    
+			latitude = location.getLatitude(); // source
+			longitude = location.getLongitude(); // source	    
 		}
 	};
 
