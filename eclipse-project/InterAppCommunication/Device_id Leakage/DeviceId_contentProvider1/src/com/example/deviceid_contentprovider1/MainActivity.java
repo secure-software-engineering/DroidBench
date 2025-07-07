199d626e8000dc77d7ee2aaf9a7b331a77b53c57
L-Jubarah-upb <leen.jubarah@upb.de>
2025-07-07 20:34:54 +0200

    Update MainActivity.java

--- eclipse-project/InterAppCommunication/Device_id Leakage/DeviceId_contentProvider1/src/com/example/deviceid_contentprovider1/MainActivity.java
+++ eclipse-project/InterAppCommunication/Device_id Leakage/DeviceId_contentProvider1/src/com/example/deviceid_contentprovider1/MainActivity.java
@@ -77,9 +77,9 @@ import android.widget.Toast;
             Toast.makeText(this, result+" no content yet!", Toast.LENGTH_LONG).show();
         }else{
             do{
-                num = c.getString(c.getColumnIndex(contentProvider.NUMBER));
+                num = c.getString(c.getColumnIndex(contentProvider.NUMBER)); // source
               /*  result = result + "\n" + c.getString(c.getColumnIndex(contentProvider.NUMBER)) +
-                " with id " +  c.getString(c.getColumnIndex(contentProvider.ID));*/
+                " with id " +  c.getString(c.getColumnIndex(contentProvider.ID));*/ 
             } while (c.moveToNext());
             //Toast.makeText(this, result, Toast.LENGTH_LONG).show();
         }
